edfc545159105c80dc9cddc1c1f3c1daa929991d
Egor I <egor.ivanov@pm.me>
2025-10-01 18:59:48 +0200

    adds delay from CLI
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- tests1.sh
+++ tests1.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 src1=$(ls src1)
 
+if [ -z $1 ]; then
+    DELAY=5
+else
+    DELAY=$1
+fi
+
 printenv | grep ALLURE_
 
 for file in $src1
 do 
     cp ./src1/${file} allure-results/
     echo "We tried ./src1/${file}"
-    sleep 20
+    echo "I'm tired, I need to cooldown for ${DELAY} seconds"
+    sleep ${DELAY}
     echo "${ALLURE_LAUNCH_URL}"
 done
 
